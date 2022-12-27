aa45df1cf84e520a584453a2a4f6060026036408
Ryan <119848808+jujudokaxing@users.noreply.github.com>
2022-12-27 20:48:19 +0800

    Update run.py

--- run.py
+++ run.py
@@ -8,42 +8,44 @@ reddit = praw.Reddit(client_id = '',
 					password = '',
 					user_agent = '',)
 
-print('Username')
-st = input('>>> ')
-reddituser = reddit.redditor(str(st))
-posts_user = reddituser.submissions.new(limit=None)
-
-user_posts = []
-subreddit_dump = []
-temp_list_subm = []
-
-print('\n\n')
-print(str(reddituser) + '  ' + ' Total Karma: ' + str(reddituser.total_karma))
-print('\n')
-
 def get_date(submission):
 	time = submission.created
 	d = datetime.datetime.fromtimestamp(time)
 	d = str(d)
 	return d
 
-for submissions in posts_user:
-	temp_list_subm.append(submissions)
-	subreddit_dump.append(submissions.subreddit)
 
-ints_list = subreddit_dump
-ints_list1 = list(set(ints_list))
-print('\tTotal Posts: ' + str(len(temp_list_subm)))
-print('\tActive Subreddits ('+  str(len(ints_list1)) + ') :  ', end='')
-for g in ints_list1:
-	print(str(g) + ', ', end='')
+while True: 
+	print('Username')
+	st = input('>>> ')
+	reddituser = reddit.redditor(str(st))
+	posts_user = reddituser.submissions.new(limit=None)
 
-print('\n\n----------------------\nPosts: \n')
+	user_posts = []
+	subreddit_dump = []
+	temp_list_subm = []
 
-for submission in temp_list_subm:
-	print(get_date(submission), end='')
-	print('\t' + str(submission.subreddit))
-	print('\t\t\t' + str(submission.title))
+	print('\n\n')
+	print(str(reddituser) + '  ' + ' Total Karma: ' + str(reddituser.total_karma))
 	print('\n')
 
-print('\n----------------------')
+	for submissions in posts_user:
+		temp_list_subm.append(submissions)
+		subreddit_dump.append(submissions.subreddit)
+
+	ints_list = subreddit_dump
+	ints_list1 = list(set(ints_list))
+	print('\tTotal Posts: ' + str(len(temp_list_subm)))
+	print('\tActive Subreddits ('+  str(len(ints_list1)) + ') :  ', end='')
+	for g in ints_list1:
+		print(str(g) + ', ', end='')
+
+	print('\n\n----------------------\nPosts: \n')
+
+	for submission in temp_list_subm:
+		print(get_date(submission), end='')
+		print('\t' + str(submission.subreddit))
+		print('\t\t\t' + str(submission.title))
+		print('\n')
+
+	print('\n----------------------\n\n')
